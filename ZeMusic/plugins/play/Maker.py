10047f9425e55cb4fac7efb847214d6ff21b9d31
Mody <143101587+Source-Ze@users.noreply.github.com>
2024-02-04 17:50:24 +0200

    Update Maker.py

--- ZeMusic/plugins/play/Maker.py
+++ ZeMusic/plugins/play/Maker.py
@@ -1,15 +1,13 @@
 import asyncio
 from pyrogram import Client, filters
-from strings import get_command
 from strings.filters import command
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 from ZeMusic import (Apple, Resso, SoundCloud, Spotify, Telegram, YouTube, app)
 
 
 @app.on_message(
-    command(["","‹ اوامر التشغيل ›"])
+    command(["‹ اوامر التشغيل ›"])
     & filters.group
-    & ~filters.edited
 )
 async def maker(client: Client, message: Message):
     await message.reply_photo(
@@ -48,9 +46,8 @@ async def maker(client: Client, message: Message):
     )
 
 @app.on_message(
-    command(["","‹ اوامر التفعيل ›"])
+    command(["‹ اوامر التفعيل ›"])
     & filters.group
-    & ~filters.edited
 )
 async def maker(client: Client, message: Message):
     await message.reply_photo(
@@ -94,7 +91,6 @@ async def maker(client: Client, message: Message):
 @app.on_message(
     command(["",""])
     & filters.group
-    & ~filters.edited
 )
 async def maker(client: Client, message: Message):
     await message.reply_photo(
